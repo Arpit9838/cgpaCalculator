181b89595abb3ec8790b51338a5e8d3991737ccf
Deepam GitHub <deepam99h@gmail.com>
2023-04-28 21:49:54 +0530

    added updated code for cgpaCalculator

---
+++ updateproject/src/cone/CGPA.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cone;
+
+/**
+ *
+ * @author Dell
+ */
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ *
+ * @author Dell
+ */
+public class CGPA {
+     public static void main(String[] args) {
+        welcome a=new welcome();
+        a.setVisible(true);
+        
+    }
+}
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
